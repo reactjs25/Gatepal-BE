c8fa054f37e5ccc38708ba39fc3af884971538ec
reactjs25 <apeksha_thakre@hyperminds.tech>
2025-11-25 15:53:40 +0530

    change camelcase

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const systemRoutes = require('./systemRoutes');
 const router = express.Router();
 
 router.use('/auth', superAdminAuthRoutes);
-router.use('/user-auth', userAuthRoutes);
+router.use('/userAuth', userAuthRoutes);
 router.use('/society', societyRoutes);
 router.use('/society-admin', societyAdminRoutes);
 router.use('/system', systemRoutes);
